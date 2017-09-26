df51178597825355e4823234a1a96e6d9477233a
Malik Robinson <maliklorenzorobinson@gmail.com>
2017-09-26 10:07:29 -0400

    updates

--- challenge_one/connection.php
+++ challenge_one/connection.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO("mysql:host=localhost;dbname=challenge_one;port=3306","root","root");
+$db = new PDO("mysql:host=localhost;dbname=mrobinson_challenge_one", "r2hstudent", "SbFaGzNgGIE8kfp");
 try {
   $results = $db->query("SELECT * FROM states");
   $states = $results->fetchAll(PDO::FETCH_ASSOC);
--- challenge_two/connection.php
+++ challenge_two/connection.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO("mysql:host=localhost;dbname=challenge_two;port=3306", "root", "root");
+$db = new PDO("mysql:host=localhost;dbname=mrobinson_challenge_two", "r2hstudent", "SbFaGzNgGIE8kfp");
 $colors = $db->query("SELECT * FROM products");
 echo "<pre>";
 var_dump($colors->fetchAll(PDO::FETCH_ASSOC));
